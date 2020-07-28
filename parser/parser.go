703fd83e7a0e5fc4351482ef95ff2748efb98244
wafuwafu13 <mariobaske@i.softbank.jp>
2020-07-28 10:59:08 +0900

    2.6.7

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"Interpreter-made-in-Go-language/ast"
 	"Interpreter-made-in-Go-language/lexer"
 	"Interpreter-made-in-Go-language/token"
+	"strconv"
 )
 
 const (
@@ -45,12 +46,28 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntergerLiteral)
 
 	return p
 }
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntergerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken} // 5
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+	
+	return lit
 }
 
 func (p *Parser) Errors() []string {
