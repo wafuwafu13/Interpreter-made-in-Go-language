202207a9e860ca2b355bf6320947f70367e02e8b
wafuwafu13 <mariobaske@i.softbank.jp>
2020-09-06 10:18:34 +0900

    3.7 return文

--- object/object.go
+++ object/object.go
@@ -8,6 +8,7 @@ const (
 	NULL_OBJ  = "NULL"
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -34,3 +35,11 @@ type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
 func (n *Null) Inspect() string  { return "null" }
+
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
